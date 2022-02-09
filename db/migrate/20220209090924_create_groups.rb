7a559b61626176d87936f143b7ad0b7e43f9f648
see-why <iyadicy@gmail.com>
2022-02-09 14:14:53 +0100

    Reference using author_id in schema

--- db/migrate/20220209090924_create_groups.rb
+++ db/migrate/20220209090924_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
       t.string :name
-      t.references :user, null: false, foreign_key: {to_table: :users}
+      t.references :author, null: false, foreign_key: {to_table: :users}
 
       t.timestamps
     end
--- db/migrate/20220209092111_create_entities.rb
+++ db/migrate/20220209092111_create_entities.rb
@@ -3,7 +3,7 @@ class CreateEntities < ActiveRecord::Migration[7.0]
     create_table :records do |t|
       t.string :name
       t.decimal :amount
-      t.references :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: {to_table: :users}
 
       t.timestamps
     end
