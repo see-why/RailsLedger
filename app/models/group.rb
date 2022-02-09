31cc613b320419e93904f30e53997d66b39cc2e0
see-why <iyadicy@gmail.com>
2022-02-09 12:16:01 +0100

    Add icon

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   validates :name, presence: true
 
+  has_one_attached :icon
   has_many :group_entities, dependent: :delete_all
   has_many :entities, through: :group_entities
   belongs_to :author, class_name: 'User'
