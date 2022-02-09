27581fabd4aa3f336fdb8fa8d5a88edae888194c
see-why <iyadicy@gmail.com>
2022-02-09 10:24:55 +0100

    Enitity model Scaffold

---
+++ app/views/entities/_entity.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! entity, :id, :name, :amount, :user_id, :created_at, :updated_at
+json.url entity_url(entity, format: :json)
---
+++ db/migrate/20220209092111_create_entities.rb
@@ -0,0 +1,11 @@
+class CreateEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entities do |t|
+      t.string :name
+      t.decimal :amount
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
