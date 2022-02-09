7ebf79a19e0463c38ece3bb1e93623fc4cb89c3e
see-why <iyadicy@gmail.com>
2022-02-09 09:49:08 +0100

    Devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "user#index"
+  devise_for :users
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
