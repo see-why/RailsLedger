1f02bbf2674196c3d3cfc67fc8841528d20ce0b8
see-why <iyadicy@gmail.com>
2022-02-10 22:25:32 +0100

    Display sum of all records for user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,6 +4,7 @@ class GroupsController < ApplicationController
   # GET /groups or /groups.json
   def index
     @groups = current_user.groups
+    @sum = current_user.records.sum(:amount)
   end
 
   # GET /groups/1 or /groups/1.json
@@ -25,7 +26,7 @@ class GroupsController < ApplicationController
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to groups_url, notice: 'Group was successfully created.' }
+        format.html { redirect_to groups_url, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @group }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +39,7 @@ class GroupsController < ApplicationController
   def update
     respond_to do |format|
       if @group.update(group_params)
-        format.html { redirect_to group_url(@group), notice: 'Group was successfully updated.' }
+        format.html { redirect_to group_url(@group), notice: 'Category was successfully updated.' }
         format.json { render :show, status: :ok, location: @group }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +53,7 @@ class GroupsController < ApplicationController
     @group.destroy
 
     respond_to do |format|
-      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
+      format.html { redirect_to groups_url, notice: 'Category was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
