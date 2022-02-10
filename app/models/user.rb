d3faa3a5fb9d2bfd36e17ca3fc9e424b9c2d4e4c
see-why <iyadicy@gmail.com>
2022-02-10 12:38:07 +0100

    User has many records not entities

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   validates :email, presence: true
 
   has_many :groups, foreign_key: 'author_id'
-  has_many :entities, foreign_key: 'author_id'
+  has_many :records, foreign_key: 'author_id'
 end
